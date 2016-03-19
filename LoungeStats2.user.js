fdf5363088af78c4252231ed70e2d868c50ee7d8
kaganus <kagan.ustungel@gmail.com>
2016-03-19 20:40:24 +0200

    fix for graph point click url

--- LoungeStats2.user.js
+++ LoungeStats2.user.js
@@ -918,7 +918,7 @@ LoungeStats.loadStats = function(){
 
 			$('#loungestats_profitgraph').bind('jqplotDataClick',
 				function (ev, seriesIndex, pointIndex) {
-					window.open('/match?m='+bets[betsKeys[pointIndex]].matchid, '_blank');
+					window.open('/match?m='+betsKeys[pointIndex], '_blank');
 				}
 			);
 
